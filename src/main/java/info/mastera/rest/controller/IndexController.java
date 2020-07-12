a3841fa3f86211116dc351ad837cb8b8909f7f14
mikhail.menko <menko.mihail@gmail.com>
2020-07-12 22:05:50 +0300

    Adding logging

--- src/main/java/info/mastera/rest/controller/IndexController.java
+++ src/main/java/info/mastera/rest/controller/IndexController.java
@@ -1,6 +1,9 @@
 package info.mastera.rest.controller;
 
+import info.mastera.HerokuApp;
 import info.mastera.model.Item;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HerokuApp.class);
+
     @GetMapping("/")
     public String index() {
+        LOGGER.info("index page");
         return "Hello World";
     }
 
     @GetMapping("/item")
     public ResponseEntity<Item> item() {
+        LOGGER.info("item page");
         return ResponseEntity.ok(new Item()
                 .setName("SimpleName"));
     }
